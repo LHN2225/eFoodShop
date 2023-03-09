1a09b62ebc0f654ccaf5b8e2c50a5e22c10a3a54
lhnhan <hoangnhan2506@gmail.com>
2023-03-09 08:47:56 +0700

    init project spring boot

---
+++ eFoodShop/src/main/java/com/example/eFoodShop/EFoodShopApplication.java
@@ -0,0 +1,13 @@
+package com.example.eFoodShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EFoodShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EFoodShopApplication.class, args);
+	}
+
+}
---
+++ eFoodShop/src/test/java/com/example/eFoodShop/EFoodShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eFoodShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EFoodShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
