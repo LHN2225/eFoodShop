5e47d2e8845e4767f679651090727238928f701f
Lucky-Star-999 <theboost1305@gmail.com>
2023-03-12 20:56:11 +0700

    Create login page

---
+++ shipper/src/main/java/com/example/shipper/controller/LoginViewController.java
@@ -0,0 +1,32 @@
+package com.example.shipper.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.example.shipper.entity.User;
+
+@Controller
+@RequestMapping("/shipper/login")
+public class LoginViewController {
+
+    @GetMapping("")
+    public String loginPage() {
+        return "login";
+    }
+
+    @PostMapping("")
+    @ResponseBody
+    public User testPassParameters(@RequestParam String username, @RequestParam String password) {
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+        return user;
+    }
+
+}
