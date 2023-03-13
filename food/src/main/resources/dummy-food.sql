8afd3f382690cf6b11db384291f2905b19225a07
lhnhan <hoangnhan2506@gmail.com>
2023-03-13 13:02:51 +0700

    Update dummy-food.sql

--- food/src/main/resources/dummy-food.sql
+++ food/src/main/resources/dummy-food.sql
@@ -1,11 +1,11 @@
-insert into food (name, description, image_url, price, is_deleted) values ('Broken rice with grilled pork', 'Vietnamese traditional dish - Com tam', 'image-food/Grilled-side-Pork.jpg', 30000, 0);
-insert into food (name, description, image_url, price, is_deleted) values ('Rice with char siu pork', 'BBQ pork with secret recipe', 'image-food/Char-Siu-Pork.jpg', 35000, 0);
-insert into food (name, description, image_url, price, is_deleted) values ('Rice with grilled chiken', 'Grilled chicken with special ginger sauce', 'image-food/Grilled-chicken-Pork.jpg', 40000, 0);
-insert into food (name, description, image_url, price, is_deleted) values ('Egg fried rice', 'Egg fried rice cooked with special oyster sauce', 'image-food/egg-fried-rice.jpg', 30000, 0);
+insert into food (name, description, image_url, price, is_deleted) values ('Broken rice with grilled pork', 'Vietnamese traditional dish - Com tam', 'Grilled-side-Pork.jpg', 30000, 0);
+insert into food (name, description, image_url, price, is_deleted) values ('Rice with char siu pork', 'BBQ pork with secret recipe', 'Char-Siu-Pork.jpg', 35000, 0);
+insert into food (name, description, image_url, price, is_deleted) values ('Rice with grilled chiken', 'Grilled chicken with special ginger sauce', 'Grilled-chicken-Pork.jpg', 40000, 0);
+insert into food (name, description, image_url, price, is_deleted) values ('Egg fried rice', 'Egg fried rice cooked with special oyster sauce', 'egg-fried-rice.jpg', 30000, 0);
 
-insert into food (name, description, image_url, price, is_deleted) values ('Beef Pho', 'Well-Done beef', 'image-food/beef-pho.jpg', 45000, 0);
-insert into food (name, description, image_url, price, is_deleted) values ('Chicken Pho', 'Steamed chicken', 'image-food/chicken-pho.jpg', 40000, 0);
-insert into food (name, description, image_url, price, is_deleted) values ('Bun cha', 'Vietnamese grilled pork with rice vermicelli noodles', 'image-food/bun-cha.jpg', 40000, 0);
-insert into food (name, description, image_url, price, is_deleted) values ('Mi Quang', 'Vietnamese Turmeric Noodles from Quang Nam', 'image-food/mi-quang.jpg', 35000, 0);
+insert into food (name, description, image_url, price, is_deleted) values ('Beef Pho', 'Well-Done beef', 'beef-pho.jpg', 45000, 0);
+insert into food (name, description, image_url, price, is_deleted) values ('Chicken Pho', 'Steamed chicken', 'chicken-pho.jpg', 40000, 0);
+insert into food (name, description, image_url, price, is_deleted) values ('Bun cha', 'Vietnamese grilled pork with rice vermicelli noodles', 'bun-cha.jpg', 40000, 0);
+insert into food (name, description, image_url, price, is_deleted) values ('Mi Quang', 'Vietnamese Turmeric Noodles from Quang Nam', 'mi-quang.jpg', 35000, 0);
 
-insert into food (name, description, image_url, price, is_deleted) values ('Bun bo Hue', 'Spicy Beef & Pork Noodle Soup', 'image-food/bun-bo.jpg', 45000, 0);
+insert into food (name, description, image_url, price, is_deleted) values ('Bun bo Hue', 'Spicy Beef & Pork Noodle Soup', 'bun-bo.jpg', 45000, 0);
