c2401bcbb6b83229c203a446127d40e6025a1c72
DuongMinhLoc <duongminhloc201@gmail.com>
2023-03-16 15:42:19 +0700

    BUG: cannot go to eureka

--- ManagerInfo/src/main/java/com/example/managerinfo/ManagerInfoApplication.java
+++ ManagerInfo/src/main/java/com/example/managerinfo/ManagerInfoApplication.java
@@ -2,8 +2,10 @@ package com.example.managerinfo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ManagerInfoApplication {
 
     public static void main(String[] args) {
